c9a0fc53cab09f3637cab434e7a02a3f57e26633
Janislud <janisludborzs2@gmail.com>
2023-11-25 14:40:34 +0200

    "adaptive fix"

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (window.getComputedStyle(x).display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleMenu = document.querySelector('.toggle-menu');
+    const menu = document.querySelector('.menu');
+
+    
+    toggleMenu.addEventListener('change', function () {
+        if (toggleMenu.checked) {
+            menu.style.display = 'flex';
+        } else {
+            menu.style.display = 'none';
+        }
+    });
+});
+
+
+{/* <img class="main-logo" src="./media/header-media/green-thumb-logo.svg" alt="green-thumb-logo">
+   
+           
+<nav class="main-navigation">
+        <ul class="list-wrapper">
+            <li><a class="sites" href="#">Home</a></li>
+            <li><a class="sites" href="#">Product</a></li>
+            <li><a class="sites" href="#">Abouts us</a></li>
+            <li><a class="sites" href="#">Contact us</a></li>
+        </ul>
+
+</nav> */}
