1dbe398cee38b73062e1d02694295f0c1071ff2d
Janislud <janisludborzs2@gmail.com>
2023-11-27 01:21:28 +0200

    "fix nav menu"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleMenu = document.querySelector('.toggle-menu');
-    const menu = document.querySelector('.menu');
+    const menu = document.querySelector('.menu-container');
 
     
     toggleMenu.addEventListener('change', function () {
@@ -12,16 +12,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-{/* <img class="main-logo" src="./media/header-media/green-thumb-logo.svg" alt="green-thumb-logo">
-   
-           
-<nav class="main-navigation">
-        <ul class="list-wrapper">
-            <li><a class="sites" href="#">Home</a></li>
-            <li><a class="sites" href="#">Product</a></li>
-            <li><a class="sites" href="#">Abouts us</a></li>
-            <li><a class="sites" href="#">Contact us</a></li>
-        </ul>
-
-</nav> */}
