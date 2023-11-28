24ee2fef93cb75e4b068043d4473f9105daa31a1
Janislud <janisludborzs2@gmail.com>
2023-11-28 14:55:48 +0200

    "last fix"

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const toggleMenu = document.querySelector('.toggle-menu');
     const menu = document.querySelector('.menu-container');
 
-    
+    if (window.innerWidth > 650) {
+        menu.style.display = 'flex';
+    }
+
     toggleMenu.addEventListener('change', function () {
         if (toggleMenu.checked) {
             menu.style.display = 'flex';
         } else {
-            menu.style.display = 'none';
+          
+            if (window.innerWidth <= 650) {
+                menu.style.display = 'none';
+            }
+        }
+    });
+
+ 
+    window.addEventListener('resize', function () {
+     
+        if (window.innerWidth > 650) {
+            menu.style.display = 'flex';
+        } else {
+           
+            if (!toggleMenu.checked) {
+                menu.style.display = 'none';
+            }
         }
     });
 });
-
